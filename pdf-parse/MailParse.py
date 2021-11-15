ec066adaed5d1d17d7280f7e8bd49103f40f5854
Cosmin Poieana <cosmin@robocorp.com>
2021-11-15 17:49:26 +0100

    Fix robot on e-mail -> docx conversion

--- pdf-parse/MailParse.py
+++ pdf-parse/MailParse.py
@@ -1,21 +1,87 @@
 import email
+import logging
 
 from htmldocx import HtmlToDocx
 from tidylib import tidy_document
 from RPA.Email.ImapSmtp import ImapSmtp
 
 
-def validate_html(content: str) -> str:
+# ---- BEGIN part of future changes in rpaframework library ---- 
+
+def _get_part_filename(msg):
+    filename = msg.get_filename()
+    if filename and decode_header(filename)[0][1] is not None:
+        filename = decode_header(filename)[0][0].decode(decode_header(filename)[0][1])
+    if filename:
+        filename = filename.replace("\r", "").replace("\n", "")
+    return filename
+
+
+def _get_decoded_email_body(message, html_first=False):
+        """Decode email body.
+
+        :param message_body: Raw 7-bit message body input e.g. from imaplib. Double
+            encoded in quoted-printable and latin-1
+        :return: Message body as unicode string and information if message has
+            attachments
+
+        Detect character set if the header is not set.
+        We try to get text/plain, but if there is not one then fallback to text/html.
+        """
+        if not message.is_multipart():
+            content_charset = message.get_content_charset()
+            text = str(
+                message.get_payload(decode=True),
+                content_charset or "utf-8",
+                "ignore",
+            )
+            return text.strip(), False
+        
+        text = ""
+        html = None
+        has_attachments = False
+        
+        for part in message.walk():
+            content_filename = _get_part_filename(part)
+            if content_filename:
+                has_attachments = True
+                continue
+
+            content_charset = part.get_content_charset()
+            if not content_charset:
+                # We cannot know the character set, so return decoded "something"
+                text = part.get_payload(decode=True)
+                continue
+
+            content_type = part.get_content_type()
+            data = str(part.get_payload(decode=True), str(content_charset), "ignore")
+            if content_type == "text/plain":
+                text = data
+            elif content_type == "text/html":
+                html = data
+
+        if html_first:
+            data = html or text
+        else:
+            data = text or html
+        return (
+            (data.strip(), has_attachments) if data else ("", has_attachments)
+        )
+
+# ---- END part of future changes in rpaframework library ---- 
+
+
+def _validate_html(content: str) -> str:
     document, errors = tidy_document(content, options={"numeric-entities": 1})
-    print("Errors: ", errors)
+    logging.debug("HTML validation errors: %s", errors)
     return document
 
 
 def email_to_dictionary(raw_email: str) -> dict:
     message = email.message_from_string(raw_email)
     message_dict = dict(message.items())
-    body, _ = ImapSmtp().get_decoded_email_body(message)
-    message_dict["Body"] = validate_html(body)
+    body, _ = _get_decoded_email_body(message, html_first=True)  # need to add this support in the library
+    message_dict["Body"] = _validate_html(body)
     return message_dict
 
 
--- pdf-parse/tasks.robot
+++ pdf-parse/tasks.robot
@@ -7,12 +7,18 @@ Library    RPA.Robocorp.WorkItems
 Library    MailParse  # local Python library
 
 
-*** Tasks ***
+*** Keywords ***
 Email To Document
-    ${mail_file} =     Get Work Item File    mail.eml
-    ${mail_data} =     Get File    ${mail_file}
+    [Arguments]    ${input_path}    ${output_path}
+    ${mail_data} =     Get File    ${input_path}
     ${mail_dict} =     Email To Dictionary    ${mail_data}
     ${mail_html} =     Set Variable    ${mail_dict}[Body]
 
-    RPA.FileSystem.Create File    ${OUTPUT_DIR}${/}mail.html    ${mail_html}    overwrite=True
-    Html To Docx    ${mail_html}    ${OUTPUT_DIR}${/}mail.docx
+    RPA.FileSystem.Create File    ${output_path}.html    ${mail_html}    overwrite=True
+    Html To Docx    ${mail_html}    ${output_path}
+
+
+*** Tasks ***
+Convert email to docx
+    ${mail_file} =     Get Work Item File    mail.eml
+    Email To Document    ${mail_file}    ${OUTPUT_DIR}${/}mail.docx
