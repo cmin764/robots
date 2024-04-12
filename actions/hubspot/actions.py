606cb838b7887c12781e5f913f514abc7a6266d7
Cosmin Poieana <cosmin@sema4.ai>
2024-04-12 12:03:16 +0400

    Add hubspot action for testing

---
+++ actions/hubspot/actions.py
@@ -0,0 +1,68 @@
+"""Set of actions operating on HubSpot resources.
+
+Currently supporting:
+- Searching: companies, contacts, deals, tasks
+"""
+
+
+import json
+import os
+from pathlib import Path
+from typing import Annotated
+
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+from robocorp.actions import Request, action
+
+from hubspot import HubSpot
+from hubspot.crm.companies import PublicObjectSearchRequest as CompanySearchRequest
+
+ACCESS_TOKEN_FIELD = "HUBSPOT_ACCESS_TOKEN"
+
+load_dotenv(Path("devdata") / ".env")
+
+
+class CompanyResult(BaseModel):
+    """Company search result object holding the queried information."""
+
+    names: Annotated[list[str], Field(description="Company names.")]
+
+
+def _get_api_client(request: Request) -> HubSpot:
+    # Sets the access token from the incoming request object if present, otherwise it
+    #  defaults to an environment variable.
+    access_token = os.getenv(ACCESS_TOKEN_FIELD)
+    context_data = request.headers.get("X-Action-Context")
+    if context_data:
+        action_context = json.loads(context_data)
+        access_token = action_context.get(ACCESS_TOKEN_FIELD, access_token)
+
+    return HubSpot(access_token=access_token)
+
+
+@action(is_consequential=False)
+def hubspot_search_companies(
+    request: Request, query: str, limit: int = 10
+) -> CompanyResult:
+    """Search for HubSpot companies based on the provided string query.
+
+    This is a basic search returning a list of company names that are matching the
+    `query` among any of their properties. The search will be limited to at most
+    `limit` results, therefore you have to increase this parameter if you want to
+    obtain more.
+
+    Args:
+        query: String that is searched for in all the company properties for a match.
+        limit: The maximum number of results the search can return.
+
+    Returns:
+        A structure with a list of company names matching the query.
+    """
+    api_client = _get_api_client(request)
+    search_request = CompanySearchRequest(query=query, limit=limit)
+    response = api_client.crm.companies.search_api.do_search(
+        public_object_search_request=search_request
+    )
+    names = [result.to_dict()["properties"]["name"] for result in response.results]
+    print(f"Companies matching query: {', '.join(names)}")
+    return CompanyResult(names=names)
